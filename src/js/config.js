40e4a0f6146738848e750f0122d7bef7d3e73f1a
snehal <snehalgote2605@gmail.com>
2022-07-28 12:21:06 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '08554131-c643-47fb-9b0d-a11877246571';
+export const MODAL_CLOSE_SEC = 2.5;
